b13865ddb4343a16ac940220609bc98776e4c4d3
yeon-a-choi <banmu_@naver.com>
2021-03-25 15:51:27 +0900

    accountList (강사님이랑 같이)
    accountList (강사님이랑 같이)

--- src/main/java/com/ee/y3/account/AccountController.java
+++ src/main/java/com/ee/y3/account/AccountController.java
@@ -1,5 +1,15 @@
 package com.ee.y3.account;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account/**")
 public class AccountController {
+	
+	@RequestMapping("accountList")
+	public void getList() throws Exception {
+		
+	}
 
 }
